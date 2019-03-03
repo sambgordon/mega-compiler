93b1e3c5357ad53edf62ca128b748a41b448eb2c
samuelbennett <samgordondev@gmail.com>
2019-03-02 20:32:28 -0600

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mega-compiler
-Project that utilizes Gulp to automatically watch and compile PostCSS, CoffeeScript, Pug, and various other tasks.
+mega-compiler utilizes Gulp to automatically watch and compile PostCSS, CoffeeScript, Pug, and run beautification tasks. Browsersync is then used to create a server and reload the page any time a change is made to the files being watched.
+
+### File structure
+
+All code to be compiled goes into `./src/pre`. PostCSS, CoffeeScript and Pug are then output to `./src/post` and finally the new `index.html` is beautified and compiled in the root directory.
+
+    ├── src                     # Folder containing all pre and post-compiled files
+    │   ├── pre                 # Contains files to be compiled  
+    │       ├── index.pug       # Pug file  
+    │       ├── script.coffee   # CoffeeScript file 
+    │       └── style.css       # PostCSS file
+    │   ├── post                # Contains files compiled using Gulp
+    │       ├── index.html      # Compiled from ./pre/*.pug  
+    │       ├── script.js       # Compiled from ./pre/*.coffee 
+    │       └── style.css       # Compiled from ./pre/*.css
+    └── gulpfile.js            
+    └── index.html              # Compiled from beautified index.html in ./post/*.html
+    └── package.json
+
+
+### Requiring modules
+    const gulp =            require('gulp'); //this project uses gulp 3.9.1 - everything else is a devDependency
+    const postcss =         require('gulp-postcss');
+    const autoprefixer =    require('gulp-autoprefixer'); //add vendor prefixes
+    const browserSync  =    require('browser-sync').create(); //use browserSync to create server and reload page when gulp is run
+    const coffee =          require('gulp-coffee');
+    const pug =             require('gulp-pug');
+    const beautify =        require('gulp-beautify'); //beautify pug output
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+/*
+
+Mega Compiler
+-----by-----
+samuelbennett
+
+edit index.html with pug via './pre/index.pug'
+edit js with coffeescript via './pre/script.coffee'
+edit css with postcss via './pre/style.css'
+
+*/
+
+const gulp =            require('gulp');
+const postcss =         require('gulp-postcss');
+const autoprefixer =    require('gulp-autoprefixer'); //add vendor prefixes
+const browserSync  =    require('browser-sync').create();
+const coffee =          require('gulp-coffee');
+const pug =             require('gulp-pug');
+const beautify =        require('gulp-beautify'); //beautify pug output
+
+//create postcss task
+gulp.task('postcss', function () {
+    return gulp.src(['src/pre/*.css'])
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+  }))
+    .pipe(gulp.dest('src/post'))
+    .pipe(browserSync.stream());
+  });
+
+  //create coffee task
+  gulp.task('coffee', function() {
+    gulp.src('src/pre/*.coffee')
+      .pipe(coffee({bare: true}))
+      .pipe(gulp.dest('src/post'))
+      .pipe(browserSync.stream());
+  });
+
+  //create pug task
+  gulp.task('pug', function buildHTML(){
+    gulp.src('src/pre/*.pug')
+      .pipe(pug())
+      .pipe(gulp.dest('src/post'))
+      .pipe(browserSync.stream());
+  });
+
+//beautify pug output
+gulp.task('beautify', function() {
+  return gulp.src('src/post/*.html')
+    .pipe(beautify.html({ indent_size: 2 }))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+//watch and serve all tasks
+  gulp.task('serve', ['postcss', 'coffee', 'pug', 'beautify'], function() {
+    browserSync.init({
+        server: "./"  
+    });
+
+    gulp.watch(['src/pre/*.css'], ['postcss']);
+    gulp.watch(['src/pre/*.coffee'], ['coffee']);
+    gulp.watch(['src/pre/*.pug'], ['pug']);
+    gulp.watch(['src/post/*.pug'], ['beautify']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// default task runs all tasks
+gulp.task('default', ['serve']);
+//
